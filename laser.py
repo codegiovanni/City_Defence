8711cffe9e9c79325bb5df2b3425b8d4db1b1170
Nicola-commits <nicolamigone179@gmail.com>
2021-12-31 14:27:12 -0800

    Refactored into multiple files, delete old version

---
+++ laser.py
@@ -0,0 +1,16 @@
+import pygame as pg
+from variables import laser_img, BLACK
+class Laser(pg.sprite.Sprite):
+    def __init__(self, x, y):
+        pg.sprite.Sprite.__init__(self)
+        self.image = laser_img
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.bottom = y
+        self.rect.centerx = x
+        self.speedy = -10
+
+    def update(self):
+        self.rect.y += self.speedy
+        if self.rect.bottom < 0:
+            self.kill()
